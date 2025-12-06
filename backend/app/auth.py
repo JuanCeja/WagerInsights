4a907fd009bf17ac03472f065ebffa821735a518
JuanCeja <jmc347ls2@yahoo.com>
2025-12-06 14:25:30 -0800

    create JWT token functions

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -33,3 +33,27 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
     """Verify a password against its hash"""
     return pwd_context.verify(plain_password, hashed_password)
 
+# JWT token functions
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    """Create a JWT access token"""
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm= ALGORITHM)
+    return encoded_jwt
+
+def verify_token(token: str, credentials_exception):
+    """Verify and decode a JWT token"""
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: int = payload.get("sub")
+        if user_id is None:
+            raise credentials_exception
+        token_data = schemas.TokenData(user_id = user_id)
+        return token_data
+    except JWTError:
+        raise credentials_exception
