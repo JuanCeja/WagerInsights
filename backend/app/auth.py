d33cb59773504a756812159d8ca3598bd926719d
JuanCeja <jmc347ls2@yahoo.com>
2025-12-06 14:15:03 -0800

    create our functions for password hashing

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -13,3 +13,23 @@ from sqlalchemy.orm import Session
 
 load_dotenv()
 
+# password hashing configuration
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated = "auto")
+
+# Oauth2 scheme for token authentication
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
+
+# JWT Settings
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7 # 1 week
+
+# Password hashing functions
+def hash_passwords(password: str) -> str:
+    """Hash plain text password"""
+    return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    """Verify a password against its hash"""
+    return pwd_context.verify(plain_password, hashed_password)
+
