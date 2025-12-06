92ec9846365683f80d259e376e4d6994eb87aee0
JuanCeja <jmc347ls2@yahoo.com>
2025-12-06 14:40:35 -0800

    Create authentication with bcrypt password hashing and JWT Tokens

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -57,3 +57,19 @@ def verify_token(token: str, credentials_exception):
         return token_data
     except JWTError:
         raise credentials_exception
+    
+# Dependency to get current users from token
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    """Get current authenticated user"""
+    credentials_exception = HTTPException(
+        status_code = status.HTTP_401_UNAUTHORIZED,
+        detail = "Could not validate credentials",
+        headers= {"WWW-authenticate": "Bearer"},
+    )
+    token_data = verify_token(token, credentials_exception)
+    user = db.query(models.User).filter(models.User.id == token_data.user_id).first()
+
+    if user is None:
+        raise credentials_exception
+    
+    return user
