77ccca2c7d725dbb6c0b0a61e39dfbc3b6c77a36
JuanCeja <jmc347ls2@yahoo.com>
2026-01-17 21:23:31 -0800

    start creating Game CRUD operations

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -1,8 +1,10 @@
+from datetime import datetime
+
 from app import models, schemas
 from app.auth import hash_password
 from sqlalchemy.orm import Session
 
-# User CRUD Operations
+# -------------------- User CRUD Operations --------------------
 
 def get_user_by_email(db: Session, email: str):
     """Get a user by email"""
@@ -37,3 +39,41 @@ def update_user_balance(db: Session, user_id: int, new_balance: float):
         db.commit()
         db.refresh(user)
     return user
+
+# -------------------- Game CRUD Operations --------------------
+
+def create_game(
+    home_team: str,
+    away_team: str, 
+    home_team_odds: float,
+    away_team_odds: float,
+    sport: str,
+    game_date: datetime,
+    status: str,
+    winner: str,
+    db: Session ):
+    
+    # ********* here in the parameters i think i need to add the Game Create Schema and maybe not all the field parametes. fix this then proceed to get all games function
+    
+    created_game = {
+        home_team: home_team,
+        away_team: away_team, 
+        home_team_odds: home_team_odds,
+        away_team_odds: away_team_odds,
+        sport: sport,
+        game_date: game_date,
+        status: status,
+        winner: winner,
+    }
+    
+    db.add(created_game)
+    db.commit()
+    db.refresh(created_game)
+
+    return created_game
+
+def get_game_by_id(db: Session, game_id: int):
+    return db.query(models.Game).filter(models.Game.id == game_id).first()
+
+def get_all_games(db. Session, )
+# -------------------- Bet CRUD Operations --------------------
