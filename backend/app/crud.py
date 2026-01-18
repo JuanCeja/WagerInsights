18469544c165604560261c1f662a81b2e8ef48d3
JuanCeja <jmc347ls2@yahoo.com>
2026-01-18 09:44:16 -0800

    Implement dynamic game filtering with SQLAlchemy query chaining

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import Optional
 
 from app import models, schemas
 from app.auth import hash_password
@@ -42,29 +43,9 @@ def update_user_balance(db: Session, user_id: int, new_balance: float):
 
 # -------------------- Game CRUD Operations --------------------
 
-def create_game(
-    home_team: str,
-    away_team: str, 
-    home_team_odds: float,
-    away_team_odds: float,
-    sport: str,
-    game_date: datetime,
-    status: str,
-    winner: str,
-    db: Session ):
+def create_game(game: schemas.GameCreate, db: Session):
     
-    # ********* here in the parameters i think i need to add the Game Create Schema and maybe not all the field parametes. fix this then proceed to get all games function
-    
-    created_game = {
-        home_team: home_team,
-        away_team: away_team, 
-        home_team_odds: home_team_odds,
-        away_team_odds: away_team_odds,
-        sport: sport,
-        game_date: game_date,
-        status: status,
-        winner: winner,
-    }
+    created_game = models.Game(**game.model_dump())
     
     db.add(created_game)
     db.commit()
@@ -75,5 +56,44 @@ def create_game(
 def get_game_by_id(db: Session, game_id: int):
     return db.query(models.Game).filter(models.Game.id == game_id).first()
 
-def get_all_games(db. Session, )
+def get_all_games(
+    home_team: Optional[str],
+    away_team: Optional[str],
+    status: Optional[str],
+    sport:Optional[str],
+    game_date: Optional[datetime],
+    winner: Optional[str],
+    settled_at: Optional[datetime],
+    created_at: Optional[datetime],
+    db: Session
+    ):
+    
+    query = db.query(models.Game)
+    
+    if home_team is not None:
+        query = query.filter(models.Game.home_team == home_team)
+    
+    if away_team is not None:
+        query = query.filter(models.Game.away_team == away_team)
+    
+    if status is not None:
+        query = query.filter(models.Game.status == status)
+    
+    if sport is not None:
+        query = query.filter(models.Game.sport == sport)
+    
+    if game_date is not None:
+        query = query.filter(models.Game.game_date == game_date)
+    
+    if winner is not None:
+        query = query.filter(models.Game.winner == winner)
+    
+    if settled_at is not None:
+        query = query.filter(models.Game.settled_at == settled_at)
+    
+    if created_at is not None:
+        query = query.filter(models.Game.created_at == created_at)
+        
+    return query.all()
+
 # -------------------- Bet CRUD Operations --------------------
