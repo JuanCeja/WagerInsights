025c0a7a203e254bad19ff52f72bfa192a34a9c3
JuanCeja <jmc347ls2@yahoo.com>
2026-06-22 16:42:53 -0700

    use VITE_API_BASE_URL env var instead of hardcoded localhost

--- frontend/src/services/gamesService.js
+++ frontend/src/services/gamesService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE_URL = "http://localhost:8000"
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
 
 export async function getGames(filters = {}) {
     const response = await axios.get(`${API_BASE_URL}/games`, {
